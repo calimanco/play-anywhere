40277d2054200076de4a286e1ae144140cc11089
calimanco <calimanco@qq.com>
2021-03-16 23:11:58 +0800

    fix: fix versionPrinter

--- src/lib/versionPrinter.ts
+++ src/lib/versionPrinter.ts
@@ -1,8 +1,10 @@
-import { isStrictNull } from '../helpers/utils'
-
 export default function versionPrinter(): void {
   console.log(
-    `v${!isStrictNull(process.env.version) ? process.env.version : 'unknown'}`
+    `v${
+      typeof process.env.version !== 'undefined'
+        ? process.env.version
+        : 'unknown'
+    }`
   )
   process.exit(1)
 }
